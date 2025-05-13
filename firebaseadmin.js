71865d6db256e35ee6227cd36aa9609f66553138
Meiliaaa <111545923+Meiliaaa@users.noreply.github.com>
2025-05-13 18:17:13 +0700

    Update firebaseadmin.js

--- firebaseadmin.js
+++ firebaseadmin.js
@@ -8,3 +8,24 @@ admin.initializeApp({
 const auth = admin.auth();
 
 module.exports = auth;
+const auth = require("./firebaseAdmin");
+
+const verifyAdmin = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1]; // Ambil token dari header
+    const decodedToken = await auth.verifyIdToken(token);
+    if (!decodedToken.admin) return res.status(403).json({ message: "Unauthorized" });
+    
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid Token" });
+  }
+};
+app.post("/products", verifyAdmin, async (req, res) => {
+  const { name, price, image_url } = req.body;
+  const result = await pool.query(
+    "INSERT INTO products (name, price, image_url) VALUES ($1, $2, $3) RETURNING *",
+    [name, price, image_url]
+  );
+  res.json(result.rows[0]);
+});
